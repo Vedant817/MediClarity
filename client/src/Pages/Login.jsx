f2459243184c26ac32054abee31bd34239b22d32
Kumar Shresth <kumarshresth2004@gmail.com>
2024-02-09 13:55:15 +0530

    login and register

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className='big-container'>
+      <div class="container">
+  <div class="card">
+    <h2 className='Login'>Login</h2>
+    <form className='form-div'>
+      <input type="text" className="username" name="username" placeholder="Username" required/>
+      <input type="password" className="password" name="password" placeholder="Password" required/>
+      <br/>
+      <button className="submit">Login</button>
+    </form>
+  </div>
+</div>
+      </div>
+      
+    </div>
   )
 }
 
