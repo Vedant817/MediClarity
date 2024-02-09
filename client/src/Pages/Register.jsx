8491f74de19c0620ec343944d2ca7fdf813afb92
Kumar Shresth <kumarshresth2004@gmail.com>
2024-02-09 21:44:20 +0530

    register frontend

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import './Register.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Register = () => {
+
+  const [username, setusername] = useState('');
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await axios.post('./register', { username,email,password });
+    setusername('');
+    setpassword('');
+    setemail('');
+}
+
+
   return (
     <div>
        <div class="container">
@@ -11,10 +27,10 @@ const Register = () => {
           <span>Already have an account? <a href="login">Sign In</a></span>
         </div>
         <div class="form">
-        <form action="/register" method="post">
-          <input type="text" name="username" id="username" placeholder="UserName" />
-          <input type="email" name="email" placeholder="Email" id="email" />
-          <input type="password" name="password" placeholder="Password" id="password" />
+        <form action="/register" method="post" onSubmit={handleSubmit}>
+          <input type="text" name="username" id="username" placeholder="UserName" onChange={e=>setusername(e.target.value)} />
+          <input type="email" name="email" placeholder="Email" id="email" onChange={e=>setemail(e.target.value)}/>
+          <input type="password" name="password" placeholder="Password" id="password" onChange={e=>setpassword(e.target.value)}/>
           <button>Sign Up</button>
           </form>
         </div>
