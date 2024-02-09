35dffc1ab46749e0add4da69a187e4977cbc6294
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-02-09 19:52:06 +0530

    Multer Middleware for File Uploading

--- server/app.js
+++ server/app.js
@@ -4,3 +4,13 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser())
+
+export {app};
