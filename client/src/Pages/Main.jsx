d41d980505bb33fab5d683099dea49df6b133076
pki03 <khurmiprateek3@gmail.com>
2024-02-10 04:17:26 +0530

    additions in main.css

--- client/src/Pages/Main.jsx
+++ client/src/Pages/Main.jsx
@@ -45,14 +45,13 @@ const Main = () => {
 
             <div className="category-name"></div> <br/>
                     
-            <div className="card-category-1">
-                        
-                <div className="basic-card basic-card-aqua">
-                    <div className="card-content">
-                        <span className="card-title"></span>
-                        <p className="card-text"><div id="preview"></div></p> {/* Render extracted text here */}
-                    </div>
-
+            <div className="card-container">
+                <div className="card-category-1">
+                    <div className="basic-card basic-card-aqua">
+                        <div className="card-content">
+                            <span className="card-title"></span>
+                            <p className="card-text"><div id="preview"></div></p> {/* Render extracted text here */}
+                        </div>
                         <div className="card-link">
                             <a href="#" title="Read Full"><span></span></a>
                         </div>
@@ -60,19 +59,17 @@ const Main = () => {
                 </div>
 
                 <div className="card-category-1">
-                        
-                <div className="basic-card basic-card-aqua">
-                    <div className="card-content">
-                        <span className="card-title"></span>
-                        <p className="card-text">{extractedText}</p> {/* Render extracted text here */}
-                    </div>
-
+                    <div className="basic-card basic-card-aqua">
+                        <div className="card-content">
+                            <span className="card-title"></span>
+                            <p className="card-text">{extractedText}</p> {/* Render extracted text here */}
+                        </div>
                         <div className="card-link">
                             <a href="#" title="Read Full"><span></span></a>
                         </div>
                     </div>
                 </div>
-            
+            </div>
         </div>
     );
 }
