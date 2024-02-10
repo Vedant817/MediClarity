11bd7eb715df11a19a5fa619befea7469e00f512
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-02-10 10:47:32 +0530

    Frontend and Backend Combine

--- image_detection/Keyword_generator.py
+++ image_detection/Keyword_generator.py
@@ -1,26 +1,18 @@
 import easyocr
 from collections import Counter
 
-# Initialize EasyOCR with desired language
 reader = easyocr.Reader(['en'])
 
-# Read the text from an image file
-result = reader.readtext('C:\\Users\\vedan\\Downloads\\MediClarity\\image_detection\\image3.jpeg')  # Replace 'your_image.png' with the path to your image file
+result = reader.readtext('C:\\Users\\vedan\\Downloads\\MediClarity\\server\\Images\\myfile.png')
 
-# Extract keywords from the detected text
 keywords = []
 for detection in result:
-    text = detection[1]  # Extract the detected text
-    # Split the text into words and filter out short words
+    text = detection[1]
     words = [word.lower() for word in text.split() if len(word) > 2]
     keywords.extend(words)
 
-# Count the frequency of each keyword
 keyword_counts = Counter(keywords)
 keywords_list=[]
 
-# Print the keywords and their frequencies
 for keyword, count in keyword_counts.items():
     keywords_list.append(keyword)
-    # print(f"{keyword}")
-    # print(prompt)
--- server/index.js
+++ server/index.js
@@ -11,19 +11,6 @@ const port = process.env.PORT || 3000;
 
 connectDB()
     .then(() => {
-        app.get('/run-python', (req, res) => {
-            const comm = 'python C:\\Users\\vedan\\Downloads\\MediClarity\\image_detection\\Keyword_generator.py'
-            const command = 'python C:\\Users\\vedan\\Downloads\\MediClarity\\image_detection\\Report_generator.py'
-            exec(command, (error, stdout, stderr) => {
-                if (error) {
-                    console.error(`Error executing Python script: ${error.message}`);
-                    res.status(500).send('Internal Server Error');
-                    return;
-                }
-                console.log(`Python script output: ${ stdout }`);
-                res.status(200).send(stdout);
-            })
-        })
         app.listen(port, () => {
             console.log(`Server Running at port: ${port}`);
         })
