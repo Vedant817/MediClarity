6c723db9d24296ba0693cc1d581fd62359cad169
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-13 16:01:05 +0530

    Navbar Completed

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,43 +1,26 @@
 import React from 'react'
-import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">MEDICLARITY</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          {/* <a class="nav-link active" aria-current="page" href="#">Home</a> */}
-        </li>
-        <li class="nav-item">
-          {/* <a class="nav-link" href="#">Link</a> */}
-        </li>
-        <li class="nav-item dropdown">
-          {/* <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a> */}
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      {/* <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-    </div>
-  </div>
-</nav>
-    </div>
+    <nav className='flex items-center justify-between border px-3 pb-1 rounded-2xl bg-neutral-800'>
+      <div className='items-start'>
+        <div className='text-5xl font-bold text-[#28bf96]'>
+          <Link to='/'>MEDICLARITY</Link>
+        </div>
+      </div>
+      <div className='flex items-center space-x-6 text-3xl font-semibold'>
+        <div className='hover:text-[#28bf96]'>
+          <Link to='/main'>Try Now</Link>
+        </div>
+        <div className='hover:text-[#28bf96]'>
+          <Link to='/about'>About</Link>
+        </div>
+        <div className='hover:text-[#28bf96]'>
+          <Link to='/contact'>Contact Us</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
