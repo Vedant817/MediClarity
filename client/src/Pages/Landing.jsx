82bf34af785f6f782c31946a3b8e479acb61aec6
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-13 16:01:21 +0530

    Landing Page Bug Fixed

--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
 import { Link } from 'react-router-dom';
-
 
 const Landing = () => {
   return (
-    <div className="w-full h-screen">
-      <nav className='flex items-center justify-between border px-3 pb-1 rounded-2xl bg-neutral-800'>
-        <div className='items-start'>
-          <div className='text-5xl font-bold text-[#28bf96]'>
-            <Link to='/'>MEDICLARITY</Link>
-          </div>
-        </div>
-        <div className='flex items-center space-x-6 text-3xl font-semibold'>
-          <div className='hover:text-[#28bf96]'>
-            <Link to='/main'>Try Now</Link>
-          </div>
-          <div className='hover:text-[#28bf96]'>
-            <Link to='/about'>About</Link>
-          </div>
-          <div className='hover:text-[#28bf96]'>
-            <Link to='/contact'>Contact Us</Link>
-          </div>
-        </div>
-      </nav>
+    <div className="w-full h-screen my-2">
+      <Navbar />
       <main className="flex items-center justify-between p-6 m-4 h-4/5">
         <div className="w-1/2">
           <div className='text-6xl font-bold my-2'>
