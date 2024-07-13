7549c92e5e7a09bcdd8dfea7db43f719150d48fc
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-13 15:41:09 +0530

    Completed Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
-import AppRoute  from './routes';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Contact from './Pages/Contact.jsx';
+import About from './Pages/About.jsx';
+import Landing from './Pages/Landing.jsx';
+import Main from './Pages/Main.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <AppRoute/>
+    <div className="App bg-neutral-900 p-2 text-white">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/main' element={<Main />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/Pages/Landing.jsx
+++ client/src/Pages/Landing.jsx
@@ -1,81 +1,53 @@
 import React from 'react';
-import './Landing.css'; // Import your CSS file
-import { useNavigate } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 
 const Landing = () => {
-
-  const navigate = useNavigate();
-  const RoutetoRegister = () =>{
-    let path = '/register';
-    navigate(path);
-  }
-
-  const RoutetoAbout = () =>{
-    let path = '/about';
-    navigate(path);
-  }
-  const RoutetoContact = ()=>
-  {
-    let path ='/contact'
-    navigate(path);
-  }
-
-  const RoutetoMain = ()=>{
-    let path = '/main'
-    navigate(path)
-  }
-
   return (
-    <div className="container">
-      <nav>
-        <div className="nav__logo">MEDICLARITY</div>
-        <ul className="nav__links">
-          <li className="link" onClick={RoutetoMain}><a href="#">Home</a></li>
-          <li className="link" onClick={RoutetoAbout}><a href="#">About Us</a></li>
-          {/* <li className="link"><a href="#">Courses</a></li> */}
-          {/* <li className="link"><a href="#">Pages</a></li> */}
-          {/* <li className="link"><a href="#">Blog</a></li> */}
-          <li className="link" onClick={RoutetoContact}><a href="#">Contact</a></li>
-        </ul>
-        <button className="btn" onClick={RoutetoRegister}>Register Now</button>
+    <div className="w-full h-screen">
+      <nav className='flex items-center justify-between border px-3 pb-1 rounded-2xl bg-neutral-800'>
+        <div className='items-start'>
+          <div className='text-5xl font-bold text-[#28bf96]'>
+            <Link to='/'>MEDICLARITY</Link>
+          </div>
+        </div>
+        <div className='flex items-center space-x-6 text-3xl font-semibold'>
+          <div className='hover:text-[#28bf96]'>
+            <Link to='/main'>Try Now</Link>
+          </div>
+          <div className='hover:text-[#28bf96]'>
+            <Link to='/about'>About</Link>
+          </div>
+          <div className='hover:text-[#28bf96]'>
+            <Link to='/contact'>Contact Us</Link>
+          </div>
+        </div>
       </nav>
-      <header className="header">
-        <div className="content">
-          <h1><span>Get Quick</span><br />Medical Services</h1>
-          <p>
-            In today's fast-paced world, access to prompt and efficient medical
-            services is of paramount importance. When faced with a medical
-            emergency or seeking immediate medical attention, the ability to
-            receive quick medical services can significantly impact the outcome
-            of a situation.
-          </p>
-          <button className="btn">Get Services</button>
+      <main className="flex items-center justify-between p-6 m-4 h-4/5">
+        <div className="w-1/2">
+          <div className='text-6xl font-bold my-2'>
+            <h1>Get Quick</h1>
+            <h1>Medical Service</h1>
+          </div>
+          <div className='py-2 mb-2'>
+            <p className='text-2xl'>
+              In today's fast-paced world, access to prompt and efficient medical
+              services is of paramount importance. When faced with a medical
+              emergency or seeking immediate medical attention, the ability to
+              receive quick medical services can significantly impact the outcome
+              of a situation.
+            </p>
+          </div>
+          <div className='bg-[#1a876a] w-1/3 rounded-2xl'>
+            <button className="bg-[#28bf96] w-full text-center font-semibold text-lg p-3 rounded-2xl hover:bg-[hsl(164,50%,27%)]">
+              <Link to='/main'>Try Now</Link>
+            </button>
+          </div>
         </div>
-        <div className="image">
-          <span className="image__bg"></span>
-          <img src="https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png" alt="header image" />
-          {/* <div className="image__content image__content__1">
-            <span><i className="ri-user-3-line"></i></span>
-            {/* <div className="details">
-              <h4>1520+</h4>
-              <p>Active Clients</p>
-            </div> */}
-          {/* </div> */}
-          {/* <div className="image__content image__content__2">
-            <ul>
-              <li>
-                <span><i className="ri-check-line"></i></span>
-                Get 20% off on every 1st month
-              </li>
-              <li>
-                <span><i className="ri-check-line"></i></span>
-                Expert Doctors
-              </li>
-            </ul>
-          </div> */}
+        <div className="w-1/2 h-full relative">
+          <img src="https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png" alt="header" className='absolute inset-0 w-full h-full object-contain' />
         </div>
-      </header>
+      </main>
     </div>
   );
 }
