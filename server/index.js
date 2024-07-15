96270280baafd2a725f2aab515d2e5d3a04debe2
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-07-15 11:53:29 +0530

    Route for extracting info

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
-import {Resend} from 'resend'
+import { Resend } from 'resend'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { PDFExtract } from 'pdf.js-extract'
+import * as fs from 'fs/promises';
+import path from 'path';
+import upload from './middlewares/multer.middleware.js'
 dotenv.config();
 
 const app = express();
-app.use(cors());
+const pdfExtract = new PDFExtract();
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
+// app.use('/api/upload-file', express.raw({ type: 'application/pdf', limit: '10mb' }));
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -48,6 +56,38 @@ app.post('/api/send-email', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while sending the email' });
     }
 });
+
+app.post('/api/upload-file', (req, res, next) => {
+    console.log('Headers: ', req.headers);
+    console.log('Request Body: ', req.body)
+    next()
+}, upload.single('file'), async (req, res) => {
+    try {
+        console.log(req)
+        if (!req.file) {
+            return res.status(400).json({ error: 'No file uploaded' });
+        }
+
+        const filepath = req.file.path;
+        console.log(filepath)
+
+        try {
+            const data = await pdfExtract.extract(filepath, {});
+            //? Delete the temporary file
+            await fs.unlink(filepath);
+            res.status(200).json(data);
+        } catch (extractError) {
+            console.error(extractError);
+            try {
+                await fs.unlink(filepath);
+            } catch (unlinkError) {
+                console.error('Failed to delete the temporary file.', unlinkError);
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while uploading the file' });
+    }
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
