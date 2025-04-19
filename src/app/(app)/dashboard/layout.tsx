0f0ad9b019593949281c02c0967285577358dbbd
Vedant Mahajan <vedantmahajan271@gmail.com>
2025-04-19 19:03:07 +0530

    Bug Fixes

--- src/app/(app)/dashboard/layout.tsx
+++ src/app/(app)/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen flex-col">
             {children}
